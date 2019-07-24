121f8fb0095501db656d2d41feea22fc1e302d23
smokyfog <1473092739@qq.com>
2019-07-25 00:16:24 +0800

    修改接口为服务器地址

--- common/common.js
+++ common/common.js
@@ -1,5 +1,5 @@
 const serverUrl = "https://www.imovietrailer.com/superhero";			// 生产环境
-const localUrl = 'http://localhost:3000'
+const localUrl = 'http://129.28.187.206:3001'
 // const serverUrl = "https://www.imovietrailer-dev.com/superhero";		// 开发环境
 
 export default{
